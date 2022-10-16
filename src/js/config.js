8518e0bbc6f2557df50639a5dfcccacd9da081f6
Abdullah Raslan <raslangi@gmail.com>
2022-10-16 15:45:58 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '03b017b5-a786-42c1-8b11-c524598545bf';
+export const MODEL_CLOSE_SEC = 2.5;
